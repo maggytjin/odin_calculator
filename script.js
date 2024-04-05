8875c826fe744b7125ad7e9c5563dfe7f7015df7
Maggy Tjin <tjin.maggy@gmail.com>
2024-04-04 22:07:18 -0700

    able to add two numbers

--- script.js
+++ script.js
@@ -3,19 +3,23 @@ var operator;
 var numTwo;
 
 function add(a, b) {
-    return a + b;
+    numOne = parseFloat(a) + parseFloat(b);
+    display.textContent = `${numOne}`;
 };
 
 function subtract(a, b) {
-    return a - b;
+    numOne = parseFloat(a) - parseFloat(b);
+    display.textContent = `${numOne}`;
 };
 
 function multiply(a, b) {
-    return a * b;
+    numOne = parseFloat(a) * parseFloat(b);
+    display.textContent = `${numOne}`;
 };
 
 function divide(a, b) {
-    return a / b;
+    numOne = parseFloat(a) / parseFloat(b);
+    display.textContent = `${numOne}`;
 };
 
 function operate(operator, numOne, numTwo) {
@@ -24,7 +28,13 @@ function operate(operator, numOne, numTwo) {
 
 var display = document.querySelector(".display");
 var displayValue = [];
+function clear() {
+    display.textContent = "";
+};
 function numberClicked(num) {
+    if (displayValue.length == 0) {
+        clear();
+    };
     displayValue.push(num);
     console.log(displayValue);
     display.textContent += `${num}`;
@@ -71,13 +81,35 @@ zero.addEventListener("click", function () {
     numberClicked(0);
 });
 
+function getOne() {
+    numOne = displayValue.join("");
+    displayValue = [];
+    console.log(`Number One:  ${numOne}`);
+};
+function getTwo() {
+    if (numOne.length != 0) {
+        numTwo = displayValue.join("");
+        displayValue = [];
+    }
+};
+
 var plus = document.querySelector(".plus");
 plus.addEventListener("click", function () {
-    numOne = displayValue.join("");
+    getOne();
+    operator = add;
 });
-
+var minus = document.querySelector(".minus");
+minus.addEventListener("click", function() {
+    getOne();
+    operator = subtract;
+})
 
 var equals = document.querySelector(".equal");
-equals.addEventListener("click", function operate(operator, numOne, numTwo) {
-    return operator(numOne, numTwo);
+equals.addEventListener("click", function () {
+    getTwo();
+    console.log(`Number One:  ${numOne}`);
+    console.log(`Number Two:  ${numTwo}`);
+    operate(operator, numOne, numTwo);
+    numOne;
+    console.log(numOne);
 });
